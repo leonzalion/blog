889ccc3d2833f9309539e3b60649fd35562228c8
Leon Si <contact@leonzalion.com>
2022-05-10 13:50:29 -0400

    chore: format

--- packages/website/src/layouts/blog-layout.vue
+++ packages/website/src/layouts/blog-layout.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import NavigationHeader from '~/components/navigation-header.vue';
 import { VueSpinnerPuff } from 'vue3-spinners';
+
+import NavigationHeader from '~/components/navigation-header.vue';
 import StreamPlayer from '~/components/stream-player.vue';
 </script>
 
--- packages/website/src/utils/markdown.ts
+++ packages/website/src/utils/markdown.ts
@@ -1,9 +1,9 @@
 import Markdown from 'markdown-it';
-import onetime from 'onetime';
-// @ts-ignore
-import markdownItTaskCheckbox from 'markdown-it-task-checkbox';
 import markdownItAnchor from 'markdown-it-anchor';
 import markdownItPrism from 'markdown-it-prism';
+// @ts-expect-error
+import markdownItTaskCheckbox from 'markdown-it-task-checkbox';
+import onetime from 'onetime';
 
 export const getMarkdownInstance = onetime(() => {
 	const md = new Markdown({
