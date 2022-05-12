7c44fde962d1f28c08b30c9a5a64d8cd39b838be
Leon Si <contact@leonzalion.com>
2022-05-11 22:10:41 -0400

    fix: compile script

--- packages/scripts/src/utils/paths.ts
+++ packages/scripts/src/utils/paths.ts
@@ -1,6 +1,5 @@
 import { getProjectDir } from 'lion-system';
-
-import * as path from 'path';
+import * as path from 'node:path';
 
 export const monorepoDir = getProjectDir(import.meta.url, {
 	monorepoRoot: true,
