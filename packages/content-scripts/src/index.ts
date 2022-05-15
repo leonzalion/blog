7acc326ca9ec3b643305fb389417e4cb9aa746a6
Leon Si <contact@leonzalion.com>
2022-05-15 17:29:08 -0400

    feat: content scripts

--- packages/content-scripts/src/index.ts
+++ packages/content-scripts/src/index.ts
@@ -1,1 +1,3 @@
 export { generateContentMetadata } from './utils/metadata.js';
+export { getNotionClient } from './utils/notion.js';
+export { getTasksFromNotion } from './utils/tasks.js';
--- packages/content-scripts/src/utils/notion.ts
+++ packages/content-scripts/src/utils/notion.ts
@@ -2,6 +2,10 @@ import { Client } from '@notionhq/client';
 import process from 'node:process';
 import onetime from 'onetime';
 
-export const getNotionClient = onetime(
-	() => new Client({ auth: process.env.NOTION_KEY })
-);
+export const getNotionClient = onetime(() => {
+	if (process.env.NOTION_KEY === undefined) {
+		throw new Error('`NOTION_KEY` not found in environment.');
+	}
+
+	return new Client({ auth: process.env.NOTION_KEY });
+});
