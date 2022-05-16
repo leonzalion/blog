030f6b36ad485144545dc8b7e4a963e6592f0a41
Leon Si <contact@leonzalion.com>
2022-05-16 15:00:44 -0400

    refactor: synchronization scripts

---
+++ packages/content-scripts/src/utils/github/update.ts
@@ -0,0 +1,81 @@
+import { dayjs } from '@leonzalion-blog/date-utils';
+import { Buffer } from 'node:buffer';
+
+import type { GitTreeItem } from '~/types/github.js';
+import { debug } from '~/utils/debug.js';
+import { getOctokit } from '~/utils/github/octokit.js';
+
+interface UpdateGithubFileProps {
+	filePath: string;
+	content: string;
+	branch: string;
+	message: string;
+}
+
+export async function updateGithubFile({
+	filePath,
+	content,
+	branch,
+	message,
+}: UpdateGithubFileProps) {
+	const octokit = getOctokit();
+
+	const owner = 'leonzalion';
+	const repo = 'blog';
+	const contentBase64 = Buffer.from(content).toString('base64');
+
+	debug(() => 'Retrieving the current branch ref...');
+	const getRefResponse = await octokit.rest.git.getRef({
+		owner,
+		repo,
+		ref: `heads/${branch}`,
+	});
+	const baseTree = getRefResponse.data.object.sha;
+
+	debug(() => 'Creating the blob...');
+	const blobResponse = await octokit.rest.git.createBlob({
+		owner,
+		repo,
+		content: contentBase64,
+		encoding: 'base64',
+	});
+
+	const treeItems: GitTreeItem[] = [];
+
+	treeItems.push({
+		path: filePath,
+		type: 'blob',
+		sha: blobResponse.data.sha,
+		mode: '100644',
+	});
+
+	console.info('Creating the tree...');
+	const createTreeResponse = await octokit.rest.git.createTree({
+		owner,
+		repo,
+		tree: treeItems,
+		base_tree: baseTree,
+	});
+
+	debug(() => 'Creating the commit...');
+	const commitResponse = await octokit.rest.git.createCommit({
+		message: `[automated] ${message} on ${dayjs()
+			.tz()
+			.format('YYYY-MM-DD h:mm A')}`,
+		owner,
+		repo,
+		tree: createTreeResponse.data.sha,
+		parents: [baseTree],
+	});
+
+	debug(() => 'Updating the ref...');
+	await octokit.rest.git.updateRef({
+		owner,
+		repo,
+		force: true,
+		ref: `heads/${branch}`,
+		sha: commitResponse.data.sha,
+	});
+
+	return commitResponse.data.sha;
+}
--- packages/content-scripts/src/utils/task-list-snapshots/update.ts
+++ packages/content-scripts/src/utils/task-list-snapshots/update.ts
@@ -1,72 +1,22 @@
 import type { TaskListSnapshot } from '@leonzalion-blog/content';
-import { dayjs } from '@leonzalion-blog/date-utils';
-import { Buffer } from 'node:buffer';
 
-import type { GitTreeItem } from '~/types/github.js';
-
-import { getOctokit } from '../github/octokit.js';
+import { updateGithubFile } from '~/utils/github/update.js';
 
 export async function updateGithubTaskListSnapshot(
 	taskListSnapshot: TaskListSnapshot
-): Promise<string> {
-	const octokit = getOctokit();
-
-	const owner = 'leonzalion';
-	const repo = 'blog';
-	const branchToUpdate = 'dev';
-
-	console.info('Retrieving the current branch ref...');
-	const getRefResponse = await octokit.rest.git.getRef({
-		owner,
-		repo,
-		ref: `heads/${branchToUpdate}`,
+): Promise<void> {
+	const content = JSON.stringify(taskListSnapshot);
+	const message = `Synchronized task list snapshot for ${taskListSnapshot.dateString}`;
+	await updateGithubFile({
+		filePath: `packages/content/task-list-snapshots/json/${taskListSnapshot.dateString}.json`,
+		branch: 'dev',
+		content,
+		message,
 	});
-	const baseTree = getRefResponse.data.object.sha;
-
-	console.info('Creating the blob...');
-	const notionTaskBlobResponse = await octokit.rest.git.createBlob({
-		owner,
-		repo,
-		content: Buffer.from(JSON.stringify(taskListSnapshot)).toString('base64'),
-		encoding: 'base64',
+	await updateGithubFile({
+		filePath: `content/task-list-snapshots/json/${taskListSnapshot.dateString}.json`,
+		branch: 'netlify',
+		content,
+		message,
 	});
-
-	const treeItems: GitTreeItem[] = [];
-
-	treeItems.push({
-		path: `packages/content/task-list-snapshots/json/${taskListSnapshot.dateString}.json`,
-		type: 'blob',
-		sha: notionTaskBlobResponse.data.sha,
-		mode: '100644',
-	});
-
-	console.info('Creating the tree...');
-	const createTreeResponse = await octokit.rest.git.createTree({
-		owner,
-		repo,
-		tree: treeItems,
-		base_tree: baseTree,
-	});
-
-	console.info('Creating the commit...');
-	const commitResponse = await octokit.rest.git.createCommit({
-		message: `[automated] Sync task list snapshot ${
-			taskListSnapshot.dateString
-		} from Notion on ${dayjs().tz().format('YYYY-MM-DD h:mm A')}`,
-		owner,
-		repo,
-		tree: createTreeResponse.data.sha,
-		parents: [baseTree],
-	});
-
-	console.info('Updating the ref...');
-	await octokit.rest.git.updateRef({
-		owner,
-		repo,
-		force: true,
-		ref: `heads/${branchToUpdate}`,
-		sha: commitResponse.data.sha,
-	});
-
-	return commitResponse.data.sha;
 }
