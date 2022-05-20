350d344eb142232d51aed5304418e1ef241310e8
Leon Si <contact@leonzalion.com>
2022-05-19 22:49:20 -0400

    fix: build script

--- packages/server/scripts/build.ts
+++ packages/server/scripts/build.ts
@@ -1,6 +1,13 @@
 import { execaCommandSync as exec } from 'execa';
-import { chProjectDir, copyPackageFiles } from 'lionconfig';
+import { chProjectDir, copyPackageFiles, updateJsonFile } from 'lionconfig';
 
 chProjectDir(import.meta.url);
 exec('tsc-alias');
 await copyPackageFiles({ commonjs: false });
+updateJsonFile('dist/package.json', [
+	[
+		'dependencies.@leonzalion-blog/content-scripts',
+		'link:../content-scripts/dist',
+	],
+	['dependencies.@leonzalion-blog/date-utils', 'link:../date-utils/dist'],
+]);
