66d451ed26098dc124c86d2897ca488eeda15c5d
Leon Si <contact@leonzalion.com>
2022-05-20 11:34:06 -0400

    feat: add daily task snapshots

--- packages/website/src/pages/daily-timeblock-page.vue
+++ packages/website/src/pages/daily-timeblock-page.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import 'github-markdown-css/github-markdown.css';
 
-import type { DailyTimeblock } from '@leonzalion-blog/content';
+import type {
+	DailyTimeblock,
+	TaskListSnapshot,
+} from '@leonzalion-blog/content';
 import { dayjs } from '@leonzalion-blog/date-utils';
+import { sort } from 'fast-sort';
 import { useRoute, useRouter } from 'vue-router';
 
 import {
@@ -10,12 +14,14 @@ import {
 	getDailyTimeblocksMetadata,
 } from '~/utils/daily-timeblock.js';
 import { getMarkdownInstance } from '~/utils/markdown.js';
+import { fetchTaskListSnapshot } from '~/utils/task-list-snapshot.js';
 
 const route = useRoute();
 const router = useRouter();
 const dateString = route.params.dateString?.toString();
 
 let dailyTimeblock = $ref<DailyTimeblock | undefined>();
+let taskListSnapshot = $ref<TaskListSnapshot | undefined>();
 (async () => {
 	const dailyTimeblocksMetadata = await getDailyTimeblocksMetadata();
 
@@ -28,8 +34,59 @@ let dailyTimeblock = $ref<DailyTimeblock | undefined>();
 		dailyTimeblock = await fetchDailyTimeblock({
 			dateString,
 		});
+		taskListSnapshot = await fetchTaskListSnapshot({
+			dateString,
+		});
 	}
 })();
+
+const taskListSnapshotMarkdown = $computed(() => {
+	if (taskListSnapshot === undefined) {
+		return '';
+	}
+
+	const sortedTasks = sort(taskListSnapshot.tasks).by({
+		asc: (task) =>
+			task.deadline === undefined
+				? Number.POSITIVE_INFINITY
+				: new Date(task.deadline),
+	});
+	let markdown = '';
+	for (const task of sortedTasks) {
+		// Skip completed tasks which were due before today
+		if (
+			task.deadline !== undefined &&
+			dayjs(task.deadline).isBefore(dateString) &&
+			task.completed
+		) {
+			continue;
+		}
+
+		if (task.completed) {
+			markdown += '- [x] ';
+		} else {
+			markdown += '- [ ] ';
+		}
+
+		markdown += task.description;
+
+		if (task.deadline === undefined) {
+			if (task.deadlineNotes !== undefined) {
+				markdown += ` (**Deadline:** _${task.deadlineNotes}_)`;
+			}
+		} else {
+			if (task.deadlineNotes === '') {
+				markdown += ` (**Deadline:** ${task.deadline})`;
+			} else {
+				markdown += ` (**Deadline:** ${task.deadline}, ${task.deadlineNotes})`;
+			}
+		}
+
+		markdown += '\n';
+	}
+
+	return markdown;
+});
 
 const md = getMarkdownInstance();
 </script>
@@ -43,6 +100,11 @@ const md = getMarkdownInstance();
 				{{ dayjs(dailyTimeblock.dateString).format('dddd, MMMM D, YYYY') }}
 			</h1>
 			<div v-html="md.render(dailyTimeblock.content)"></div>
+
+			<template v-if="taskListSnapshot !== undefined">
+				<h2>Task List Snapshot</h2>
+				<div v-html="md.render(taskListSnapshotMarkdown)"></div>
+			</template>
 		</div>
 	</div>
 </template>
