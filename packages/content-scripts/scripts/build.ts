35a72e752eedd24e8af156d3ff4551b8c5e50e78
Leon Si <contact@leonzalion.com>
2022-05-19 23:06:17 -0400

    feat: adjust for deployment scripts

--- packages/content-scripts/scripts/build.ts
+++ packages/content-scripts/scripts/build.ts
@@ -1,9 +1,6 @@
 import { execaCommandSync as exec } from 'execa';
-import { chProjectDir, copyPackageFiles, updateJsonFile } from 'lionconfig';
+import { chProjectDir, copyPackageFiles } from 'lionconfig';
 
 chProjectDir(import.meta.url);
 exec('tsc-alias');
 await copyPackageFiles({ commonjs: false });
-updateJsonFile('dist/package.json', [
-	['dependencies.@leonzalion-blog/date-utils', 'link:../date-utils/dist'],
-]);
--- packages/server/scripts/build.ts
+++ packages/server/scripts/build.ts
@@ -1,13 +1,6 @@
 import { execaCommandSync as exec } from 'execa';
-import { chProjectDir, copyPackageFiles, updateJsonFile } from 'lionconfig';
+import { chProjectDir, copyPackageFiles } from 'lionconfig';
 
 chProjectDir(import.meta.url);
 exec('tsc-alias');
 await copyPackageFiles({ commonjs: false });
-updateJsonFile('dist/package.json', [
-	[
-		'dependencies.@leonzalion-blog/content-scripts',
-		'link:../content-scripts/dist',
-	],
-	['dependencies.@leonzalion-blog/date-utils', 'link:../date-utils/dist'],
-]);
