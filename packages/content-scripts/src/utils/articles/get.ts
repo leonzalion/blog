ede98dbb5147576fc6b039b3a92418c0a7fe6cf4
Leon Si <contact@leonzalion.com>
2022-05-31 22:22:45 -0400

    chore: update content scripts

--- packages/content-scripts/src/utils/articles/get.ts
+++ packages/content-scripts/src/utils/articles/get.ts
@@ -1,8 +1,6 @@
 import type { Article } from '@leonzalion-blog/content';
-import got from 'got';
 import type { ValueOf } from 'type-fest';
 
-import { retrieveGithubFiles } from '~/utils/github/files.js';
 import { getNotionClient, getNotionToMarkdown } from '~/utils/notion.js';
 
 export async function getArticleFromNotion({
@@ -66,36 +64,4 @@ export async function getArticleFromNotion({
 		datePublished: properties['Date Published'].date!.start,
 		lastEditedDate: properties['Last Edited'].last_edited_time,
 	};
-}
-
-export async function getArticleFromGithub({
-	articleSlug,
-}: {
-	articleSlug: string;
-}): Promise<Article | undefined> {
-	try {
-		const articleResponse = await retrieveGithubFiles({
-			path: `content/articles/json/${articleSlug}.json`,
-			branch: 'netlify',
-		});
-
-		if (Array.isArray(articleResponse)) {
-			throw new TypeError('Expected a path to a file, not a folder');
-		}
-
-		if (articleResponse.download_url === null) {
-			throw new Error('`download_url` was not present on `articleResponse`');
-		}
-
-		const article = await got(articleResponse.download_url).json<Article>();
-
-		return article;
-	} catch (error: unknown) {
-		const err = error as { status: number };
-		if (err.status === 404) {
-			return undefined;
-		} else {
-			throw error;
-		}
-	}
 }
